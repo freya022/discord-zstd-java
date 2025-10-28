524dccb6ab2521dd278abca630018537a9f78da1
freya02 <41875020+freya022@users.noreply.github.com>
2025-10-28 22:41:59 +0100

    First docs

--- api/src/main/java/dev/freya02/discord/zstd/api/ZstdDecompressor.java
+++ api/src/main/java/dev/freya02/discord/zstd/api/ZstdDecompressor.java
@@ -2,6 +2,12 @@ package dev.freya02.discord.zstd.api;
 
 import org.jspecify.annotations.NullMarked;
 
+/**
+ * A Zstandard streaming decompressor for Discord gateway messages.
+ * <br>Each message needs to be passed in the same order they were received, none must be skipped.
+ *
+ * <p>Note: Instances are <b>not</b> thread safe, as there should be an instance per gateway connection, which uses 1 read thread.
+ */
 @NullMarked
 public interface ZstdDecompressor {
     void reset();
