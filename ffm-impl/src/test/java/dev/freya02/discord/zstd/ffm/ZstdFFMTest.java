2d774d33767b3b9fcd5a968700863f980224aff6
freya02 <41875020+freya022@users.noreply.github.com>
2025-11-26 11:38:26 +0100

    api, ffm: Add ZstdContext with input stream support

--- ffm-impl/src/test/java/dev/freya02/discord/zstd/ffm/ZstdFFMTest.java
+++ ffm-impl/src/test/java/dev/freya02/discord/zstd/ffm/ZstdFFMTest.java
@@ -8,24 +8,23 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ZstdFFMTest {
 
     private static List<TestChunks.Chunk> chunks;
 
     @BeforeAll
-    public static void setup() {
+    public static void setup() throws IOException {
         chunks = TestChunks.get(TestChunks.Compression.ZSTD);
+        ZstdNativesLoader.loadFromJar();
     }
 
     @Test
-    public void test_decompression() throws IOException {
-        assertTrue(ZstdNativesLoader.loadFromJar());
-
+    public void test_decompression() {
         ZstdDecompressorFactory factory = new ZstdFFMDecompressorFactoryProvider().get(ZstdDecompressor.DEFAULT_BUFFER_SIZE);
         ZstdDecompressor decompressor = factory.create();
         for (TestChunks.Chunk chunk : chunks) {
@@ -33,5 +32,19 @@ public class ZstdFFMTest {
             final byte[] expected = chunk.getDecompressed();
             assertArrayEquals(expected, actual);
         }
+    }
+
+    @Test
+    public void test_input_stream() throws IOException {
+        final ZstdFFMContext context = new ZstdFFMContext();
+        for (TestChunks.Chunk chunk : chunks) {
+            try (InputStream stream = context.createInputStream(chunk.getCompressed())) {
+                final byte[] actual = stream.readAllBytes();
+                final byte[] expected = chunk.getDecompressed();
+                assertArrayEquals(expected, actual);
+            }
+        }
+
+        context.close();
     }
 }
