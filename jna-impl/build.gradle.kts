4afa712d75daba5a03974aa0b9f424d78c5b1d21
freya02 <41875020+freya022@users.noreply.github.com>
2025-12-05 14:40:01 +0100

    Disable JNA impl publishing

--- jna-impl/build.gradle.kts
+++ jna-impl/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     `java-conventions`
     `java-library`
-    `publish-conventions`
 }
 
 val fullProjectName = "${rootProject.name}-${project.name}"
@@ -48,12 +47,4 @@ tasks.withType<Test> {
             excludeTestsMatching("ZstdJNATest")
         }
     }
-}
-
-registerPublication(
-    name = fullProjectName,
-    description = "Zstandard streaming decompression API for JVM Discord API wrappers using Java Native Access (JNA)",
-    url = "https://github.com/freya022/discord-zstd-java/tree/master/jna-impl",
-) {
-    from(components["java"])
 }
--- jna-impl/src/main/java/dev/freya02/discord/zstd/jna/ZstdJNADecompressorFactoryProvider.java
+++ jna-impl/src/main/java/dev/freya02/discord/zstd/jna/ZstdJNADecompressorFactoryProvider.java
@@ -1,13 +1,11 @@
 package dev.freya02.discord.zstd.jna;
 
 import dev.freya02.discord.zstd.api.ZstdDecompressorFactory;
-import dev.freya02.discord.zstd.api.ZstdDecompressorFactoryProvider;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
-public class ZstdJNADecompressorFactoryProvider implements ZstdDecompressorFactoryProvider {
+public class ZstdJNADecompressorFactoryProvider {
 
-    @Override
     public ZstdDecompressorFactory get(int bufferSizeHint) {
         return new ZstdJNADecompressorFactory(bufferSizeHint);
     }
