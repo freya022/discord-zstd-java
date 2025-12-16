a835a7def9a22b9f4a05321e55f3154c80c2b5fd
freya02 <41875020+freya022@users.noreply.github.com>
2025-12-16 11:38:52 +0100

    benchmarks: Improve results table

--- benchmarks/src/jmh/java/ResultConverter.java
+++ benchmarks/src/jmh/java/ResultConverter.java
@@ -40,47 +40,55 @@ void main(String[] args) throws IOException {
 
 private static void printDeserializationTable(BenchmarkResults zlibBulkResults, BenchmarkResults zstdBulkResults, BenchmarkResults zlibStreamResults, BenchmarkResults zstdStreamResults) {
     System.out.println("## Deserialization (input -> `DataObject`)");
-    System.out.println("| Metric averages | Bulk Zlib | Bulk Zstd | Streaming Zlib | Streaming Zstd |");
+    System.out.println("| Metric averages | Bulk Zlib (baseline) | Bulk Zstd | Streaming Zlib | Streaming Zstd |");
     System.out.println("|---------------------------|--------------------|-------------------|-------------------|-------------------|");
-    System.out.printf("|Time per operation|%s|%s|%s|%s|\n", getScoreString(zlibBulkResults.primaryMetric), getScoreString(zstdBulkResults.primaryMetric), getScoreString(zlibStreamResults.primaryMetric), getScoreString(zstdStreamResults.primaryMetric));
+    System.out.printf("|Time per operation|%s|%s|%s|%s|\n", getScoreString(zlibBulkResults.primaryMetric, zlibBulkResults.primaryMetric), getScoreString(zstdBulkResults.primaryMetric, zlibBulkResults.primaryMetric), getScoreString(zlibStreamResults.primaryMetric, zlibBulkResults.primaryMetric), getScoreString(zstdStreamResults.primaryMetric, zlibBulkResults.primaryMetric));
     for (Map.Entry<String, BenchmarkResults.Metric> entry : zlibBulkResults.secondaryMetrics.entrySet()) {
         final String metricName = entry.getKey();
-        final BenchmarkResults.Metric metric = entry.getValue();
-        System.out.printf("|%s|%s|%s|%s|%s|\n", metricName, getScoreString(metric), getScoreString(zstdBulkResults, metricName), getScoreString(zlibStreamResults, metricName), getScoreString(zstdStreamResults, metricName));
+        final BenchmarkResults.Metric baselineMetric = entry.getValue();
+        System.out.printf("|%s|%s|%s|%s|%s|\n", metricName, getScoreString(baselineMetric, baselineMetric), getScoreString(zstdBulkResults, metricName, baselineMetric), getScoreString(zlibStreamResults, metricName, baselineMetric), getScoreString(zstdStreamResults, metricName, baselineMetric));
     }
     System.out.println();
 }
 
 private static void printDecompressionTable(BenchmarkResults zlibBulkResults, BenchmarkResults zstdBulkResults, BenchmarkResults zlibStreamResults, BenchmarkResults zstdStreamResults) {
     System.out.println("## Decompression (decompresses and blackholes the data)");
-    System.out.println("| Metric averages | Bulk Zlib | Bulk Zstd | Streaming Zlib | Streaming Zstd |");
+    System.out.println("| Metric averages | Bulk Zlib (baseline) | Bulk Zstd | Streaming Zlib | Streaming Zstd |");
     System.out.println("|---------------------------|-------------------|-------------------|-------------------|-------------------|");
-    System.out.printf("|Time per operation|%s|%s|%s|%s|\n", getScoreString(zlibBulkResults.primaryMetric), getScoreString(zstdBulkResults.primaryMetric), getScoreString(zlibStreamResults.primaryMetric), getScoreString(zstdStreamResults.primaryMetric));
+    System.out.printf("|Time per operation|%s|%s|%s|%s|\n", getScoreString(zlibBulkResults.primaryMetric, zlibBulkResults.primaryMetric), getScoreString(zstdBulkResults.primaryMetric, zlibBulkResults.primaryMetric), getScoreString(zlibStreamResults.primaryMetric, zlibBulkResults.primaryMetric), getScoreString(zstdStreamResults.primaryMetric, zlibBulkResults.primaryMetric));
     for (Map.Entry<String, BenchmarkResults.Metric> entry : zlibBulkResults.secondaryMetrics.entrySet()) {
         final String metricName = entry.getKey();
-        final BenchmarkResults.Metric metric = entry.getValue();
-        System.out.printf("|%s|%s|%s|%s|%s|\n", metricName, getScoreString(metric), getScoreString(zstdBulkResults, metricName), getScoreString(zlibStreamResults, metricName), getScoreString(zstdStreamResults, metricName));
+        final BenchmarkResults.Metric baselineMetric = entry.getValue();
+        System.out.printf("|%s|%s|%s|%s|%s|\n", metricName, getScoreString(baselineMetric, baselineMetric), getScoreString(zstdBulkResults, metricName, baselineMetric), getScoreString(zlibStreamResults, metricName, baselineMetric), getScoreString(zstdStreamResults, metricName, baselineMetric));
     }
     System.out.println();
 }
 
-private static String getScoreString(BenchmarkResults results, String metricName) {
+private static String getScoreString(BenchmarkResults results, String metricName, BenchmarkResults.Metric baselineMetric) {
     var metric = results.secondaryMetrics.get(metricName);
+    return getScoreString(metric, baselineMetric);
+}
+
+private static String getScoreString(BenchmarkResults.Metric metric, BenchmarkResults.Metric baselineMetric) {
     if (metric == null) return "N/A";
 
+    var baselineDiff = getBaselineDiffString(metric, baselineMetric);
     if ("NaN".equals(metric.scoreError)) {
-        return "%.3f %s".formatted(metric.score, metric.scoreUnit);
+        return "%.3f %s%s".formatted(metric.score, metric.scoreUnit, baselineDiff);
     } else {
-        return "%.3f ± %.3f %s".formatted(metric.score, Double.valueOf(metric.scoreError), metric.scoreUnit);
+        return "%.3f ± %.3f %s%s".formatted(metric.score, Double.valueOf(metric.scoreError), metric.scoreUnit, baselineDiff);
     }
 }
 
-private static String getScoreString(BenchmarkResults.Metric metric) {
-    if (metric == null) return "N/A";
-
-    if ("NaN".equals(metric.scoreError)) {
-        return "%.3f %s".formatted(metric.score, metric.scoreUnit);
+private static String getBaselineDiffString(BenchmarkResults.Metric metric, BenchmarkResults.Metric baselineMetric) {
+    var baselineDiff = -(100 - (100 / baselineMetric.score * metric.score));
+    String signum;
+    if (Math.signum(baselineDiff) == 0.0) {
+        return "";
+    } else if (Math.signum(baselineDiff) == 1.0) {
+        signum = "+";
     } else {
-        return "%.3f ± %.3f %s".formatted(metric.score, Double.valueOf(metric.scoreError), metric.scoreUnit);
+        signum = "-";
     }
+    return " (%s%.2f%%)".formatted(signum, Math.abs(baselineDiff));
 }
